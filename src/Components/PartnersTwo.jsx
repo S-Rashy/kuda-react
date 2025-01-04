69ac238dd7e08e0b1c02f8354d5b9a2ed387191c
S-Rashy <sadiqrashidah@gmail.com>
2025-01-04 23:59:09 +0100

    edit

--- src/Components/PartnersTwo.jsx
+++ src/Components/PartnersTwo.jsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
-// import Valar from "../assets/partners2/valar.svg"
+import Valar from "../assets/partners2/Valar.svg"
 import Entree from "../assets/partners2/entre.svg"
 import Sbi from "../assets/partners2/sbi.svg"
 import Target from "../assets/partners2/target.svg"
-import Visa from "../assets/partners2/visa.svg"
+import Visa from "../assets/partners2/Visa.svg"
 
 const PartnersTwo = () => {
     return ( <PartnersTwoDiv>
         <h2>Our Partners</h2>
         <PartnerLogo>
-            {/* <img src={Valar} alt="Valar" /> */}
+            <img src={Valar} alt="Valar" />
             <img src={Entree} alt="Entree" />
             <img src={Sbi} alt="Sbi" />
             <img src={Target} alt="Target" />
